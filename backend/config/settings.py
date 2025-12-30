bb79f760dc446efd10ad8dfc4ee205503c9caa0b
Ashwani Rai <raiashwani151104@gmail.com>
2025-12-30 13:33:22 +0530

    Debug: Enable DEBUG mode to trace 500 error

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True # config('DEBUG', default=False, cast=bool) # TEMPORARY FOR DEBUGGING
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.onrender.com').split(',')
 
